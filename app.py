fafed6892037c60014889fc778ea46ebb0d2d305
Carlos <carmor_014@hotmail.com>
2020-08-26 18:05:03 -0500

    fixed modification 2

--- app.py
+++ app.py
@@ -14,9 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Uso de sqlalchemy
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity) # /auth
 
@@ -32,5 +29,6 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__=='__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug = True)
